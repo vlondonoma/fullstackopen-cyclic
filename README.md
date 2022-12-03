7ee173d1dfb5d97f558f585bba8db55685b86e16
Valentina Londoño Marin <vlondono@boostworld.com>
2022-12-03 15:44:55 -0500

    Firt version to Cyclic

---
+++ README.md
@@ -0,0 +1,5 @@
+# Full Stack Open
+Cyclic (Heroku alterative) - Solution of exercises proposed by course [Full Stack Open](https://fullstackopen.com/es) - 2022
+
+## Part 3
+- [phonebook in Cyclic](https://phonebook-vlondono.herokuapp.com/)
---
+++ index.js
@@ -0,0 +1,112 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const Person = require('./models/person')
+
+const app = express()
+
+morgan.token('body', req => {
+    return JSON.stringify(req.body)
+  })
+
+app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
+app.use(morgan(':method :url :status :response-time ms :body'))
+
+app.get('/info', (req, res) => {
+    Person.find({})
+        .then(persons => {
+            res.send(
+                "<p>Phonebook has info for "+persons.length+" people</p>"+
+                "<p>"+new Date().toLocaleString()+"</p>"
+            )
+        })
+        .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'Name or number missing' 
+    })
+  }
+
+    const personModel = new Person({
+        name: body.name,
+        number: body.number,
+    })
+
+    personModel.save()
+    .then(savedPerson => {
+        response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons', (req, res, next) => {
+    Person.find({})
+        .then(persons => {
+            res.json(persons)
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    if (!body.number) {
+      return response.status(400).json({ 
+        error: 'Number missing' 
+      })
+    }
+  
+    Person.findByIdAndUpdate(request.params.id,{number:body.number},{new:true})
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+   response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
